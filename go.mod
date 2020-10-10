69673d20281056622675700d6b45faf49a8b1e48
w1lldone <wildan.arrahman@gmail.com>
2020-10-10 17:55:33 +0700

    Dockerfile, docker-compose, configurations

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/go-redis/redis/v8 v8.2.3
+	github.com/spf13/viper v1.7.1
 	golang.org/x/net v0.0.0-20201009032441-dbdefad45b89 // indirect
 )
--- main.go
+++ main.go
@@ -1,34 +1,51 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"os"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	topic := flag.String("topic", "", "The topic name to/from which to publish/subscribe")
-	broker := flag.String("broker", "tcp://iot.eclipse.org:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
-	password := flag.String("password", "", "The password (optional)")
-	user := flag.String("user", "", "The User (optional)")
-	id := flag.String("id", "testgoid", "The ClientID (optional)")
-	cleansess := flag.Bool("clean", true, "Set Clean Session (default true)")
-	qos := flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
-	flag.Parse()
-
-	if *topic == "" {
-		fmt.Println("Invalid setting for -topic, must not be empty")
-		return
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %s", err))
 	}
-	
+
+	redisServer := viper.GetString("redis.server")
+	redisPassword := viper.GetString("redis.password")
+	redisDb := viper.GetInt("redis.db")
+	redisChannel := viper.GetString("redis.channel")
+
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisServer,
+		Password: redisPassword,
+		DB:       redisDb,
+	})
+
+	topic := viper.GetString("mqtt.topic")
+	broker := viper.GetString("mqtt.broker")
+	password := viper.GetString("mqtt.password")
+	user := viper.GetString("mqtt.user")
+	id := viper.GetString("mqtt.id")
+	cleansess := viper.GetBool("mqtt.cleaness")
+	qos := viper.GetInt("mqtt.qos")
+
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(*broker)
-	opts.SetClientID(*id)
-	opts.SetUsername(*user)
-	opts.SetPassword(*password)
-	opts.SetCleanSession(*cleansess)
+	opts.AddBroker(broker)
+	opts.SetClientID(id)
+	opts.SetUsername(user)
+	opts.SetPassword(password)
+	opts.SetCleanSession(cleansess)
 
 	choke := make(chan [2]string)
 
@@ -41,7 +58,7 @@ func main() {
 		panic(token.Error())
 	}
 
-	if token := client.Subscribe(*topic, byte(*qos), nil); token.Wait() && token.Error() != nil {
+	if token := client.Subscribe(topic, byte(qos), nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
@@ -49,5 +66,9 @@ func main() {
 	for {
 		incoming := <-choke
 		fmt.Printf("RECEIVED TOPIC: %s MESSAGE: %s\n", incoming[0], incoming[1])
+		err := rdb.Publish(ctx, redisChannel, incoming[1]).Err()
+		if err != nil {
+			panic(err)
+		}
 	}
 }
